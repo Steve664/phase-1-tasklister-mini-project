0c44313fd489e9ad568346bfe00f489b831fd7b1
Steve Kariuki <stevekariuki5@gmail.com>
2022-12-04 22:25:48 +0300

    Completed Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let newTask;
+  let priority;
+  //handles form submit event
+  const myForm = document.querySelector("#create-task-form")
+  myForm.addEventListener('submit',
+    (event) => {
+      event.preventDefault();
+      //gets value from inpt field
+      newTask = event.target.querySelector('#new-task-description').value
+      priority = event.target.querySelector('#priority option:checked').value
+      //calls function to add task to list
+      let list = document.createElement("li")
+      let ul = document.querySelector("#tasks")
+      ul.appendChild(list)
+      list.textContent = newTask
+      list.style.color = priority
+    })
 });
